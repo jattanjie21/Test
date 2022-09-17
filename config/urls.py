39f8a2555215e9b39d5764d7b9cd9f7ca79da22c
jattanjie21 <laboyjatta@gmail.com>
2022-09-17 15:20:37 +0000

    bugfix

--- config/urls.py
+++ config/urls.py
@@ -46,10 +46,10 @@ if settings.DEBUG:
         ),
         path("500/", default_views.server_error),
     ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
+    # if "debug_toolbar" in settings.INSTALLED_APPS:
+    #     import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    #     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
 
 
 admin.site.site_header =  "Gambia Participates"
